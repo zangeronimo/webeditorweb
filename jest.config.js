de6c05ffc76f47c396eb43ae4e93af6b668c1d19
Luciano Zangeronimo <zangeronimo@gmail.com>
2023-11-22 20:56:09 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/Application/Services/**/*.{ts}',
+    '<rootDir>/src/Presentation/Components/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  tranform: {
+    '.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+
+root.render(<h1>Hello World</h1>)
