24665a0359daf42abd94ac54a194ffeed935b85a
Luciano Zangeronimo <zangeronimo@gmail.com>
2023-11-23 21:44:39 -0300

    chore: create a simple web server to execute builded code

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,53 +1,53 @@
-const { DefinePlugin } = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: true,
             },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ],
       },
     ],
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
     devMiddleware: {
       writeToDisk: true,
     },
     static: {
-      directory: "./public",
+      directory: './public',
     },
     historyApiFallback: true,
     port: 8080,
   },
   plugins: [
     new DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://locahost:4000"),
+      'process.env.API_URL': JSON.stringify('http://localhost:4000'),
     }),
     new HtmlWebpackPlugin({
-      template: "./template.dev.html",
+      template: './template.dev.html',
     }),
   ],
-});
+})
