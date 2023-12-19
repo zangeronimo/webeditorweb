8a116aac25bed75dfe0fa70a3ce8c8d20ceee4e0
Luciano Zangeronimo <zangeronimo@gmail.com>
2023-12-19 17:35:40 -0300

    feat: [26] basic file upload

--- src/presentation/pages/culinary/recipe/form/index.tsx
+++ src/presentation/pages/culinary/recipe/form/index.tsx
@@ -7,6 +7,8 @@ import {
   Select,
   type SelectData,
 } from '@/presentation/components/form'
+import { Upload } from '@/presentation/components/form/upload'
+import { Group } from '@/presentation/components/group'
 import { useModal } from '@/presentation/hooks/useModal'
 import { useState, type FormEvent } from 'react'
 
@@ -25,73 +27,90 @@ export const Form = ({
   data,
   _recipeService,
 }: Props): JSX.Element => {
-  const [, setState] = useState({ reRender: false })
+  const [state, setState] = useState({ imageUpload: '' })
   const { closeModal } = useModal()
 
   const handleNewRegister = (e: FormEvent): void => {
     e.preventDefault()
     _recipeService
-      .save(data)
+      .save({ ...data, imageUpload: state.imageUpload })
       .then(() => {
         handleClearPayload()
-        setState(old => ({ reRender: !old.reRender }))
         closeModal()
       })
       .catch(e => {
         console.error(e.message)
       })
+      .finally(() => {
+        setState(old => ({ ...old, imageUpload: '' }))
+      })
   }
 
   return (
     <form onSubmit={handleNewRegister}>
-      <Input label="id" name="id" defaultValue={data.id} hidden />
-      <Select
-        label="Categories"
-        name="categoryId"
-        value={data.categoryId}
-        onChange={handleChangePayload}
-        data={[{ label: 'Select one', value: '' }, ...categories]}
-      />
-      {!!data.id && (
+      <Group>
+        <Input label="id" name="id" defaultValue={data.id} hidden />
+        <Select
+          label="Categories"
+          name="categoryId"
+          value={data.categoryId}
+          onChange={handleChangePayload}
+          data={[{ label: 'Select one', value: '' }, ...categories]}
+        />
+        <Select
+          label="Status"
+          name="active"
+          value={data.active}
+          onChange={handleChangePayload}
+          data={[
+            { label: 'Active', value: 1 },
+            { label: 'Inactive', value: 0 },
+          ]}
+        />
+      </Group>
+      <Group>
+        {!!data.id && (
+          <Input
+            label="Slug"
+            name="slug"
+            value={data.slug}
+            onChange={handleChangePayload}
+          />
+        )}
         <Input
-          label="Slug"
-          name="slug"
-          value={data.slug}
+          name="name"
+          label="Name"
+          value={data.name}
           onChange={handleChangePayload}
         />
-      )}
-      <Input
-        name="name"
-        label="Name"
-        value={data.name}
-        onChange={handleChangePayload}
-      />
-      <Editor
-        data={data.ingredients}
-        onChange={data => {
-          handleChangePayload({
-            target: { name: 'ingredients', value: data },
-          } as any)
-        }}
-      />
-      <Editor
-        data={data.preparation}
-        onChange={data => {
-          handleChangePayload({
-            target: { name: 'preparation', value: data },
-          } as any)
-        }}
-      />
-      <Select
-        label="Status"
-        name="active"
-        value={data.active}
-        onChange={handleChangePayload}
-        data={[
-          { label: 'Active', value: 1 },
-          { label: 'Inactive', value: 0 },
-        ]}
-      />
+      </Group>
+      <Group direction="column">
+        <Editor
+          data={data.ingredients}
+          onChange={data => {
+            handleChangePayload({
+              target: { name: 'ingredients', value: data },
+            } as any)
+          }}
+        />
+        <Editor
+          data={data.preparation}
+          onChange={data => {
+            handleChangePayload({
+              target: { name: 'preparation', value: data },
+            } as any)
+          }}
+        />
+      </Group>
+      <Group direction="column">
+        <Upload
+          label="Upload a new image"
+          name="imageUpload"
+          callback={(imageUpload: string) => {
+            setState(old => ({ ...old, imageUpload }))
+          }}
+        />
+      </Group>
       <Save />
     </form>
   )
